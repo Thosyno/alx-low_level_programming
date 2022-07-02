87bea5d0d0cd8df8f029452f52b75bdea10f0c03
Thosyno <asiyanbitosin@gmail.com>
2022-07-02 12:03:47 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - alphabet in lowercase, and then uppercase
+ *
+ *
+ * Return: returns zero at the end
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
