38547f452c93e7487a18bd3da403ec65f0ee2e1b
Thosyno <asiyanbitosin@gmail.com>
2022-07-05 11:55:06 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,9 @@
 /**
  * print_alphabet - Prints the alphabet in lowercase, followed by a new line
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
-	char c = 'a';
+	char.c = 'a';
 
 	while (c <= 'z')
 	{
