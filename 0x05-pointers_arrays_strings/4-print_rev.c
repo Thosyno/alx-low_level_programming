273485e7c063d303a265bfc5f9c971d97f0cd140
Thosyno <asiyanbitosin@gmail.com>
2022-07-12 16:20:48 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,10 +7,10 @@
  */
 void print_rev(char *s)
 {
-	int i, len = strlen(str);
+	int i, len = strlen(s);
 
 	for (i = len - 1; i >= 0; i--)
-		_putchar(str[i]);
+		_putchar(s[i]);
 
 	_putchar('\n');
 }
