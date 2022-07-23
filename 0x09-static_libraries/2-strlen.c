3b0c3e0386b13e8952b59fbfe27bf9e1eb9d3f8a
Thosyno <asiyanbitosin@gmail.com>
2022-07-23 06:19:32 -0700

    Static library created

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string to get the length of.
+ *
+ * Return: The length of string.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
