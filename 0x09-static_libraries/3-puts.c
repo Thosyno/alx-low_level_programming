281fd1e77bbd085e1a65a7139907ea5bc8827cf5
Thosyno <asiyanbitosin@gmail.com>
2022-07-23 11:37:52 -0700

    static library created

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,17 @@
 #include "main.h"
 
 /**
- * _puts - Prints a string to stdout
- * @str: The string to be printed.
+ * _puts - Prints string
+ * @str: Input string
+ * Return: no string
  */
 void _puts(char *str)
 {
-	while (*str)
-		_putchar(*str++);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
 
 	_putchar('\n');
 }
